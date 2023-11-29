580b8ca2025ecf2533ae24d5538edc1d80d604a4
MindyBots <mindybots2023@outlook.com>
2023-11-29 15:46:37 +0530

    Added some files and made changes in existing file

---
+++ ctc-membership/src/Pages/Contact.tsx
@@ -0,0 +1,232 @@
+import React, { useState } from 'react';
+import {
+  Button,
+  TextField,
+  Typography,
+  FormControl,
+  Grid,
+  InputLabel,
+  Select,
+  MenuItem
+} from '@mui/material';
+import { MuiTelInput } from 'mui-tel-input'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { countries } from 'countries-list';
+
+
+export default function Contact(){
+
+  //EMAIL
+    const [email, setEmail] = useState('');
+    const [error2, setError2] = useState('');
+    const handleEmailChange = (event: { target: { value: any; }; }) => {
+      const newEmail = event.target.value;
+      setEmail(newEmail);
+      // Regular expression for validating email format
+      const emailPattern = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+      if (newEmail === '' || emailPattern.test(newEmail)) {
+        setError2('');
+      } else {
+        setError2('Invalid email format');
+      }
+    };
+    const handleKeyPress = (event: { key: string; preventDefault: () => void; }) => {
+      if (event.key === 'Enter' && error2) {
+        event.preventDefault();
+      }
+    };
+
+  //PHONENUMBER
+    const [value, setValue] = React.useState('')
+    const handleChange = (newValue: React.SetStateAction<string>) => {
+      setValue(newValue)
+    }
+
+  //NATIVEPLACE
+    const [name, setName] = useState('');
+    const namePattern = /^[A-Za-z]+$/; // Regular expression pattern for names (only alphabets allowed)
+    const handleNameChange = (event: { target: { value: any; }; }) => {
+      const inputValue = event.target.value;
+      if (namePattern.test(inputValue)) {
+        setName(inputValue);
+      }
+    };
+
+  //CITY
+    const [city, setCity] = useState('');
+    const cityPattern = /^[A-Za-z]+$/;
+    const handleCityChange = (event: { target: { value: any; }; }) => {
+      const inputValue = event.target.value;
+      if (cityPattern.test(inputValue)) {
+        setCity(inputValue);
+      }
+    };
+
+  //STATE
+    const [state, setState] = useState('');
+    const statePattern = /^[A-Za-z]+$/;
+    const handleStateChange = (event: { target: { value: any; }; }) => {
+      const inputValue = event.target.value;
+      if (statePattern.test(inputValue)) {
+        setState(inputValue as string);
+      }
+    };
+
+  //ZIP
+    const [zip, setZip] = useState('');
+    const zipPattern = /^[0-9]+$/;
+    const handleZipChange = (event: { target: { value: any; }; }) => {
+      const inputValue = event.target.value;
+      if (zipPattern.test(inputValue)) {
+        setZip(inputValue);
+      }
+    };
+
+  //COUNTRY
+    const [, setCountry] = useState('');
+    const countryPattern = /^[A-Za-z]+$/;
+    const handleCountryChange = (event: { target: { value: any; }; }) => {
+      const inputValue = event.target.value;
+      if (countryPattern.test(inputValue)) {
+        setCountry(inputValue as string);
+      }
+    };
+    const defaultCountry = 'US';
+
+    
+    return(
+     <>
+        <Typography variant='h6'>Contact Details</Typography>
+        <Grid container spacing={1}>
+          <Grid item xs={12} md={6}>
+            <TextField
+              fullWidth
+              margin='normal'
+              label="Email"
+              variant="outlined"
+              value={email}
+              onChange={handleEmailChange}
+              onKeyDown={handleKeyPress}
+              error={Boolean(error2)}
+              helperText={error2}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <FormControl fullWidth margin='normal'>
+              <MuiTelInput label="Phone Number" fullWidth value={value} onChange={handleChange} defaultCountry='US' />
+            </FormControl>
+          </Grid>
+        </Grid>
+        <Grid container spacing={1}>
+          <Grid item xs={12} md={6}>
+            <TextField
+              margin='normal'
+              label="Address Line 1"
+              variant="outlined"
+              fullWidth
+              type="address"
+              required
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              margin='normal'
+              label="Address Line 2"
+              variant="outlined"
+              fullWidth
+              type="address"
+            />
+          </Grid>
+        </Grid>
+        <Grid container spacing={1}>
+          <Grid item xs={12} md={6}>
+            <TextField
+              fullWidth
+              margin='normal'
+              label='City'
+              value={city}
+              onChange={handleCityChange}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              fullWidth
+              margin='normal'
+              label='State'
+              value={state}
+              onChange={handleStateChange}
+            />
+          </Grid>
+        </Grid>
+        <Grid container spacing={1}>
+          <Grid item xs={12} md={6}>
+            <TextField
+              margin='normal'
+              label="Zip"
+              variant="outlined"
+              fullWidth
+              type="Zip"
+              onChange={handleZipChange}
+              value={zip}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <FormControl fullWidth margin='normal'>
+              <InputLabel id="country-dropdown-label">Select Country</InputLabel>
+              <Select
+                label="Select Country"
+                labelId="country-dropdown-label"
+                id="country-dropdown"
+                onChange={handleCountryChange}
+                defaultValue={defaultCountry}
+              >
+                {Object.entries(countries).map(([code, country]) => (
+                  <MenuItem key={code} value={code}>
+                    {country.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Grid>
+        </Grid>
+        <Grid container spacing={1}>
+          <Grid item xs={12} md={6}>
+            <TextField
+              margin='normal'
+              label="Native Place"
+              variant="outlined"
+              fullWidth
+              value={name}
+              onChange={handleNameChange}
+              inputProps={{
+                pattern: namePattern.source, // Set the regular expression pattern
+              }}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <FormControl fullWidth margin='normal'>
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DatePicker
+                  label="Membership Since"
+                  slotProps={{ textField: { size: 'medium' } }}
+                  views={['year']}
+                />
+              </LocalizationProvider>
+            </FormControl>
+          </Grid>
+        </Grid>
+        <br></br>
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          sx={{my:2}}
+        >
+          Register
+        </Button>
+    </>
+    );
+}
---
+++ ctc-membership/src/Pages/Member.tsx
@@ -0,0 +1,270 @@
+import React, { useState } from 'react';
+import { Box, TextField, Button, Select, MenuItem, IconButton, Typography, FormControl, InputLabel, Grid, styled } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import { Person } from '@mui/icons-material';
+
+interface Person {
+  id: number;
+  firstName: string;
+  lastName: string;
+  relationship: string;
+  value: string;
+  selectedOption: string;
+  textInput: string;
+  option: string;
+  dob: string;
+  gradeLevel: string;
+}
+
+const ShrinkingButton = styled(Button)({
+  transition: 'all 0s ease-in-out',
+  '&.shrinked': {
+    width: '140px', 
+    fontSize: '10px',
+    marginRight: '13px'
+  },
+});
+
+const Member: React.FC = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [uniqueKey, setUniqueKey] = useState(0);
+  const isNameValid = (name: string) => /^[A-Za-z]+$/.test(name);
+  const isNameValid1 = (name1: string) => /^[A-Za-z]+$/.test(name1);
+  const isNumberValid = (number: string) => /^[1-12]+$/.test(number);
+
+  const [shrinked, setShrinked] = useState(false);
+
+  const handleButtonClick = () => {
+    setShrinked(true);
+  };
+
+
+ //BOXES
+  const handleAddButtonClick = () => {
+    const newPerson: Person = {
+      id: Date.now(),
+      firstName: '',
+      lastName: '',
+      relationship: '',
+      value: '',
+      selectedOption: '',
+      textInput: '',
+      option: '',
+      dob: '',
+      gradeLevel: ''
+    };
+    setPeople((prevPeople) => [...prevPeople, newPerson]);
+    setUniqueKey((prevId) => prevId + 1);
+  };
+  const handleRemoveBox = (id: number) => {
+    setPeople((prevPeople) => {
+      const updatedPeople = prevPeople.filter((person) => person.id !== id);
+      if (updatedPeople.length === 0) {
+        setShrinked(false); // Set shrinked to false if no box components are left
+      }
+      return updatedPeople;
+    });
+  };
+
+ //NAMES
+  const handleTextFieldChange = (id: string, field: 'firstName' | 'lastName') => (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = event.target.value.trim();
+    if (field === 'lastName') {
+      const personWithSameFirstName = people.find((person) => person.id === Number(id));
+      if (personWithSameFirstName && personWithSameFirstName.firstName === newValue) {
+        // Last Name cannot be the same as First Name
+        return;
+      }
+    }
+    if (isNameValid(newValue)) {
+      setPeople((prevPeople) =>
+        prevPeople.map((person) =>
+          person.id === Number(id) ? { ...person, [field]: newValue } : person
+        )
+      );
+    }
+  };
+
+ //RELATIONSHIP
+  const handleInputChange = (id: number, field: keyof Person, value: string) => {
+    setPeople((prevPeople) =>
+      prevPeople.map((person) =>
+        person.id === id ? { ...person, [field]: value } : person
+      )
+    );
+  };
+
+ //SELECTED OPTION
+  const handleTextInputChange = (id: string, field: 'textInput') => (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value;
+    if (isNameValid1(inputValue)) {
+      setPeople((prevPeople) =>
+        prevPeople.map((person) =>
+          person.id === Number(id) ? { ...person, [field]: inputValue } : person
+        )
+      );
+    }
+  };
+
+ //DATE OF BIRTH
+  const calculateAge = (dob: string): number => {
+    const birthDate = new Date(dob);
+    const currentDate = new Date();
+    const age = currentDate.getFullYear() - birthDate.getFullYear();
+
+    // Check if the birthday has occurred this year
+    if (currentDate.getMonth() < birthDate.getMonth() ||
+        (currentDate.getMonth() === birthDate.getMonth() && currentDate.getDate() < birthDate.getDate())) {
+      return age - 1;
+    }
+
+    return age;
+  };
+  const handleInputChange1 = (id: number, field: keyof Person, value: string) => {
+    setPeople((prevPeople) =>
+      prevPeople.map((person) =>
+        person.id === id ? { ...person, [field]: value } : person
+      )
+    );
+  };
+
+ //GRADE LEVEL
+  const handleInputChange2 = (id: string, field: 'gradeLevel') => (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const number = event.target.value;
+    const numericValue = parseInt(number, 10);
+    if (!isNaN(numericValue) && numericValue >= 1 && numericValue <= 12) {
+      if (isNumberValid(number)) {
+        setPeople((prevPeople) =>
+          prevPeople.map((person) =>
+            person.id === Number(id) ? { ...person, [field]: number } : person
+          )
+        );
+      }
+    }
+  };
+
+  const handleMultipleClicks = () =>{
+      handleAddButtonClick();
+      handleButtonClick();
+  };
+
+  return (
+    <div>
+      {people.map((person) => (
+        <Box key={person.id} sx={{ position: 'relative', marginBottom: 6 }}>
+          <IconButton
+            sx={{ position: 'absolute', top: 0, right: 0 }}
+            onClick={() => handleRemoveBox(person.id)}
+          >
+            <CloseIcon />
+          </IconButton>
+            <Typography variant="h6" sx={{display:'inline-block'}}>Member</Typography>
+            <Grid container spacing={1}>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  label="First Name"
+                  value={person.firstName}
+                  onChange={handleTextFieldChange(String(person.id), 'firstName')}
+                  fullWidth
+                  margin='normal'
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  label="Last Name"
+                  value={person.lastName}
+                  onChange={handleTextFieldChange(String(person.id), 'lastName')}
+                  fullWidth
+                  margin='normal'
+                />
+              </Grid>
+            </Grid>
+            <Grid container spacing={1}>
+              <Grid item xs={12} md={6}>
+                <FormControl fullWidth margin='normal'>
+                  <InputLabel id="select-label">Relationship</InputLabel>
+                  <Select
+                    label="Select Option"
+                    value={person.selectedOption}
+                    onChange={(e) => handleInputChange(person.id, 'selectedOption', e.target.value)}
+                    fullWidth
+                  >
+                    <MenuItem value={'option-1'}>Husband</MenuItem>
+                    <MenuItem value={'option-2'}>Wife</MenuItem>
+                    <MenuItem value={'option-3'}>Son</MenuItem>
+                    <MenuItem value={'option-4'}>Daughter</MenuItem>
+                    <MenuItem value={'option-5'}>Father</MenuItem>
+                    <MenuItem value={'option-6'}>Mother</MenuItem>
+                    <MenuItem value={'option-7'}>Brother</MenuItem>
+                    <MenuItem value={'option-8'}>Sister</MenuItem>
+                    <MenuItem value={'option-9'}>Others</MenuItem>
+                    {/* Add more options as needed */}
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                {person.selectedOption === 'option-9' && (
+                  <TextField
+                    label="Enter Your relationship"
+                    value={person.textInput}
+                    onChange={handleTextInputChange(String(person.id), 'textInput')}
+                    fullWidth
+                    margin='normal'
+                    key={`textfield-${uniqueKey}`}
+                  />
+                )}
+              </Grid>
+            </Grid>
+            <Grid container spacing={1}>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  label="Date of Birth"
+                  type="date"
+                  value={person.dob}
+                  onChange={(e) => handleInputChange1(person.id, 'dob', e.target.value)}
+                  fullWidth
+                  margin='normal'
+                  InputLabelProps={{
+                    shrink: true,
+                }}
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                {calculateAge(person.dob) < 18 && (
+                  <TextField
+                    label="Grade Level"
+                    value={person.gradeLevel}
+                    onChange={handleInputChange2(String(person.id), 'gradeLevel')}
+                    fullWidth
+                    margin='normal'
+                  />
+                )}
+              </Grid>
+            </Grid>
+        </Box>
+      ))}
+      <Grid container spacing={1}>
+        {shrinked && (
+          <Grid item xs={5.3} sx={{display:'inline-block'}}>
+          </Grid>
+        )}
+        <Grid item xs={shrinked ? 6 : 12} sx={{display:'inline-block'}}>
+          <ShrinkingButton
+            variant="contained"
+            color="primary"
+            sx={{flexWrap : 'wrap'}}
+            onClick={handleMultipleClicks}
+            className={shrinked ? 'shrinked' : ''}
+            fullWidth
+          >
+            Add Family Member
+          </ShrinkingButton>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Member;
