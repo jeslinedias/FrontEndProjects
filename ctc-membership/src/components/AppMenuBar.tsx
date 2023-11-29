7ae95a545b9aa725df54d3a9d3254a6ada2ad4a1
MindyBots <mindybots2023@outlook.com>
2023-11-29 15:45:11 +0530

    Made some changes to the existing files

--- ctc-membership/src/components/AppMenuBar.tsx
+++ ctc-membership/src/components/AppMenuBar.tsx
@@ -1,120 +1,42 @@
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
+//import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
+import IconButton from "@mui/material/IconButton";
+//import { Avatar } from "@mui/material";
+//import MenuIcon from "@mui/icons-material/Menu";
 
-const pages = ["Products", "Pricing", "Blog"];
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
-
-function ResponsiveAppBar() {
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
-    null
-  );
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
-    null
-  );
-
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
+export default function AppHeaderBar() {
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="logo"
+            sx={{ mr: 2 }}
           >
-             Chicago Tamil Catholics
-          </Typography>
+            <img
+              className="mui-img"
+              aria-setsize={3}
+              src="src\icons\ctclong_logo.webp"
+              alt="src\icons\ctc_vertical.png"
+              sizes="small"
+            />
 
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            {/* <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu> */}
-          </Box>
-          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
+            {/* <MenuIcon /> */}
+          </IconButton>
+          {/* <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Chicago Tamil Catholics
           </Typography> */}
           <Button color="inherit" aria-label="right">
             Login
           </Button>
         </Toolbar>
-    </AppBar>
+      </AppBar>
+    </Box>
   );
 }
---
+++ ctc-membership/src/components/EmailField.tsx
@@ -0,0 +1,52 @@
+import { TextField } from "@mui/material";
+import React from "react";
+
+export default function EmailField() {
+  const [email, setEmail] = React.useState("");
+  const [error, setError] = React.useState("");
+
+  const validateEmail = (input: string) => {
+    // Regular expression for a valid email address
+    const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+
+    if (!emailRegex.test(input)) {
+      return "Invalid email address";
+    }
+
+    return "";
+  };
+
+  const handleEmailChange = (
+    event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const inputEmail = event.currentTarget.value;
+    const errorMessage = validateEmail(inputEmail);
+    setError(errorMessage);
+    setEmail(inputEmail);
+  };
+
+  const handleOnBlur = (
+    event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    event.preventDefault();
+
+    if (!error) {
+      // Do something with the valid email address
+      console.log("Valid email:", email);
+    }
+  };
+
+  return (
+    <TextField
+      label="Email"
+      variant="outlined"
+      fullWidth
+      required
+      value={email}
+      onChange={handleEmailChange}
+      error={Boolean(error)}
+      helperText={error}
+      onBlur={handleOnBlur}
+    />
+  );
+}
